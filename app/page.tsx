7d3a0af87d159baa96b5198aa9c034a06eb84fdf
samuelbelo <samuel.belo@al.infnet.edu.br>
2023-05-25 17:52:02 +0100

    lets goo

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,23 @@
 'use client';
-import Image from 'next/image';
-import { Input } from './components/ui/input';
-import { Button } from './components/ui/button';
-import { useEffect, useState } from 'react';
-import { db } from '@/db/db';
-import { todos } from '@/db/schema/schema';
+import {Input} from './components/ui/input';
+import {Button} from './components/ui/button';
+import {useEffect, useState} from 'react';
 
 export default function Home() {
-  const [taskName, setTaskName] = useState<string>('');
-  const test = async () => {
-    return await db.select().from(todos);
-  };
+	const [taskName, setTaskName] = useState<string>('');
 
-  useEffect(() => {
-    test().then(res => console.log(res));
-  }, []);
-
-  return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='flex'>
-        <Input value={taskName} onChange={event => setTaskName(event.target.value)} />
-        <Button
-          onClick={() => {
-            console.log(taskName);
-          }}
-        >
-          Add
-        </Button>
-      </div>
-    </main>
-  );
+	return (
+		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
+			<div className='flex'>
+				<Input value={taskName} onChange={event => setTaskName(event.target.value)}/>
+				<Button
+					onClick={() => {
+						console.log(taskName);
+					}}
+				>
+					Add
+				</Button>
+			</div>
+		</main>
+	);
 }
--- drizzle/0000_cheerful_dazzler.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `todos` (
-	`id` serial AUTO_INCREMENT PRIMARY KEY NOT NULL,
-	`name` text,
-	`description` varchar(256));
